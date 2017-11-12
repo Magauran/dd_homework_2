3ec1c83e8c07b8ba7336fdfc2c90c22393d60421
Magauran <a7ls@ya.ru>
2017-11-12 23:52:52 +0300

    Change table design

--- ColorText/ListOfTextViewController.m
+++ ColorText/ListOfTextViewController.m
@@ -24,27 +24,46 @@
 
 
 - (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.textWithColor.count;
+    return self.textWithColor.count * 2;
 }
 
 
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    TextWithRange *textWithRange = [self.textWithColor objectAtIndex:indexPath.row];
+    TextWithRange *textWithRange = [self.textWithColor objectAtIndex:indexPath.row / 2];
 
     [self.delegate removeForegroundColorAttributeWithRange: textWithRange.range];
-    [self.textWithColor removeObjectAtIndex:indexPath.row];
+    [self.textWithColor removeObjectAtIndex:indexPath.row / 2];
     
     [tableView reloadData];
 }
 
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    static NSString *tableIdentifier = @"colorTextCell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:tableIdentifier];
-    TextWithRange *textWithRange = [self.textWithColor objectAtIndex:indexPath.row];
-    cell.textLabel.attributedText = textWithRange.text;
-    cell.textLabel.numberOfLines = 0;
-    return cell;
+    if (indexPath.row % 2 == 0) {
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"colorTextCell"];
+        TextWithRange *textWithRange = [self.textWithColor objectAtIndex:indexPath.row / 2];
+        cell.textLabel.attributedText = textWithRange.text;
+        cell.textLabel.numberOfLines = 0;
+        
+        
+        [cell.contentView.layer setCornerRadius:10.0f];
+        [cell.contentView.layer setMasksToBounds:YES];
+        [cell.contentView.layer setBackgroundColor:[UIColor colorWithRed:245/255.0 green:245/255.0 blue:245/255.0 alpha:1].CGColor];
+        return cell;
+    } else {
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"separatorCell"];
+        return cell;
+    }
 }
+
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.row % 2 == 0) {
+        return UITableViewAutomaticDimension;
+    } else {
+        return 10;
+    }
+}
+
 
 @end
